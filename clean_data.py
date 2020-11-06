3c878cdea0b14c17148e79d04117f46b6292f2e4
guy-ham <guy-ham@campus.technion.ac.il>
2020-11-06 17:01:44 +0200

    fix conflict

--- clean_data.py
+++ clean_data.py
@@ -18,7 +18,6 @@ def rm_ext_and_nan(CTG_features, extra_feature):
     """
     return {k: v.dropna() for k, v in CTG_features.drop(columns=extra_feature).apply(pd.to_numeric, errors='coerce').items()}
 
-
 def nan2num_samp(CTG_features, extra_feature):
     """
     :param CTG_features: Pandas series of CTG features
@@ -46,7 +45,9 @@ def sum_stat(c_feat):
     :return: Summary statistics as a dicionary of dictionaries (called d_summary) as explained in the notebook
     """
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
+    d_summary = {}
+    for k,v in c_feat.items():
+        d_summary[k] = {"min": v.min(),"Q1": v.quantile(q=0.25), "median": v.median(), "Q3": v.quantile(q=0.75), "max": v.max()}
     # -------------------------------------------------------------------------
     # return d_summary
     pass
@@ -61,7 +62,12 @@ def rm_outlier(c_feat, d_summary):
     """
     c_no_outlier = {}
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
+    for k,v in c_feat.items():
+        IQR_1_5 = 1.5*(d_summary[k]["Q3"]-d_summary[k]["Q1"])
+        c_no_outlier[k]=pd.Series([])
+        for ind,val in v.iteritems():
+            if d_summary[k]["Q1"]-IQR_1_5< val <d_summary[k]["Q3"]+IQR_1_5:
+                c_no_outlier[k] = c_no_outlier[k].add(pd.Series(val, index=[ind]),fill_value=0)
     # -------------------------------------------------------------------------
     return pd.DataFrame(c_no_outlier)
 
